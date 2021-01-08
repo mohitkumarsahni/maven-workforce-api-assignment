8bdbbf15f80c262982e958ae1d1b43a2f4db9fc8
Mohit Sahni <mohitkumarsahni@gmail.com>
2021-01-08 15:27:11 +0530

    NotNull Constraint in Reset Integer.

--- src/main/java/org/mavenworkforce/pojos/requests/other/ResetIntegerRequest.java
+++ src/main/java/org/mavenworkforce/pojos/requests/other/ResetIntegerRequest.java
@@ -5,11 +5,14 @@ import lombok.Setter;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class ResetIntegerRequest {
 
+    @NotNull(message = "The value can not be null.")
     @Min(value = 0L, message = "The value must be positive.")
     private Long current;
 }
